3f0ae2973a199b432d0ab53078433fceeed731ac
Paul Aigbokhai Olukayode <paulaigbokhaiolukayode@USERs-MacBook-Pro.local>
2024-02-05 13:44:38 +0100

    feature: add account opening

--- db/account_opening_schema.py
+++ db/account_opening_schema.py
@@ -4,7 +4,6 @@ from .database import Base
 
 class AccountOpeningSchema(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, index=True)
     email = Column(String, unique=True, index=True)
--- di/dependencies.py
+++ di/dependencies.py
@@ -1,6 +1,5 @@
 from fastapi import Depends
 from sqlalchemy.orm import Session
-
 from db.database import get_db
 from services.account_opening_service import AccountOpeningService
 
